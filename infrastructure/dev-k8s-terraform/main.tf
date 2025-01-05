0bc66fdc86bae83292df3fc87d7aa6c93e009cdd
sezzgi <sezozrn@gmail.com>
2025-01-05 02:06:21 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -168,7 +168,7 @@ resource "aws_instance" "kube-master" {
     instance_type = "t3a.medium"
     iam_instance_profile = aws_iam_instance_profile.petclinic-master-server-profile.name
     vpc_security_group_ids = [aws_security_group.petclinic-kube-master-sg.id, aws_security_group.petclinic-mutual-sg.id]
-    key_name = "firstkey"
+    key_name = "clarus"
     subnet_id = "subnet-025c24da1c8e51411"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
@@ -184,7 +184,7 @@ resource "aws_instance" "worker-1" {
     ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t3a.medium"
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
-    key_name = "firstkey"
+    key_name = "clarus"
     subnet_id = "subnet-025c24da1c8e51411"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
@@ -200,7 +200,7 @@ resource "aws_instance" "worker-2" {
     ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t3a.medium"
     vpc_security_group_ids = [aws_security_group.petclinic-kube-worker-sg.id, aws_security_group.petclinic-mutual-sg.id]
-    key_name = "firstkey"
+    key_name = "clarus"
     subnet_id = "subnet-025c24da1c8e51411"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
